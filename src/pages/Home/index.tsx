14979b51f23ecd07be4432f68b3f82ea2b22da9c
José Augusto <joseaugusto0408@gmail.com>
2023-11-18 10:59:14 -0300

    mudando title da pagina

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -30,12 +30,17 @@ export function Home(){
   });
   const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
   useEffect(() =>{
+    let interval: number;
+
     if(activeCycle) {
-      setInterval(() => {
+      interval =  setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         )
       },1000)
+    }
+    return () => {
+      clearInterval(interval)
     }
   },[activeCycle])
 
@@ -49,6 +54,7 @@ export function Home(){
       }
       setCycles(state=> [...state, newCycle])
       setActiveCycleId(id)
+      setAmountSecondsPassed(0);
       reset();
   }
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 :0;
@@ -56,7 +62,12 @@ export function Home(){
   const minutsAmount = Math.floor(currentSeconds / 60)
   const secondsAmount = currentSeconds % 60
   const minutes = String(minutsAmount).padStart(2, '0');
-  const seconds = String(secondsAmount).padStart(2,'0')
+  const seconds = String(secondsAmount).padStart(2,'0');
+  useEffect(() =>{
+    if(activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  },[minutes, seconds, activeCycle])
   
   const task = watch('task');
   const isSubmitDisabled = !task;
