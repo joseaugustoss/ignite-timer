bfb1b0fdaa4bc57715f10c571d19678137dc76e6
José Augusto <joseaugusto0408@gmail.com>
2024-06-18 11:03:58 -0300

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {ThemeProvider} from 'styled-components';
 
 import { Button } from "./components/Button";
 import {defaultTheme} from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
@@ -10,6 +11,7 @@ export function App() {
       <Button variant="secondary"/>
       <Button variant="danger"/>
       <Button variant="success"/>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
