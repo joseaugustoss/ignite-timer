15fe34dfcce055225ca4a66d7720f1ca13dd62bd
José Augusto <joseaugusto0408@gmail.com>
2024-06-18 18:26:12 -0300

    Layout de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "styled-components";
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Router } from "./Router";
@@ -8,9 +8,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-  <Router />
-  </BrowserRouter>
-  <GlobalStyle />
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
